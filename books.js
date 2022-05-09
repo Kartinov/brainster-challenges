9bbff9d0f93189776cf4c8ab728ef60417eac47b
Dimche Kartinov <dimchekartinov@gmail.com>
2022-05-09 11:50:32 +0200

    Starter code

---
+++ books.js
@@ -0,0 +1,32 @@
+export const books = [
+    {
+        title: 'The Hobbit',
+        author: 'J.R.R. Tolkien',
+        maxPages: 200,
+        onPage: 60,
+    },
+    {
+        title: 'Harry Potter',
+        author: 'J.K. Rowling',
+        maxPages: 250,
+        onPage: 150,
+    },
+    {
+        title: '50 Shades of Grey',
+        author: 'E.L. James',
+        maxPages: 150,
+        onPage: 150,
+    },
+    {
+        title: 'Don Quixote',
+        author: 'Miguel de Cervantes',
+        maxPages: 350,
+        onPage: 300,
+    },
+    {
+        title: 'Hamlet',
+        author: 'William Shakespeare',
+        maxPages: 550,
+        onPage: 550,
+    },
+];
