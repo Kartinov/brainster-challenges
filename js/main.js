ea1ba5325f23f30db5c3b7397ebdb3dd0d9fa6c9
Dimche Kartinov <dimchekartinov@gmail.com>
2022-05-18 12:03:09 +0200

    Loader added and it hides when questions are fetched

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-console.log($)
+window.addEventListener('load', () => {
+    getQuestions('https://opentdb.com/api.php?amount=20');
+});
+
+async function getQuestions(apiUrl) {
+    const response = await fetch(apiUrl); // storing response
+    const apiData = await response.json(); // storing data in form of JSON
+
+    hideLoader();
+    showStartScreen();
+}
+
+function hideLoader() {
+    document.getElementById('loader').classList.add('hide');
+}
+
+function showStartScreen() {
+    document.getElementById('start-screen').classList.add('show');
+}
