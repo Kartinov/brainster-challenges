49d810f6e74ccfb67dfab162860f0855d44425b8
Dimche Kartinov <dimchekartinov@gmail.com>
2022-06-17 10:56:14 +0200

    Homepage designed

--- business-casual/resources/views/home.blade.php
+++ business-casual/resources/views/home.blade.php
@@ -39,6 +39,59 @@
             </nav>
         </header>
 
+        <main class="pb-5">
+            {{-- Hero Section --}}
+            <section class="container py-5">
+                <div class="row">
+                    <div class="col-12 position-relative">
+                        <div class="image-wrapper w-75 ml-auto">
+                            <img src="{{ asset('images/cafee-store.jpg') }}" class="img-fluid rounded">
+                        </div>
+                        <div class="pop-up text-box text-center rounded p-3">
+                            <h3 class="text-uppercase h6">lorem ipsum</h3>
+                            <h2 class="text-uppercase h4 mb-3">lorem ipsum</h2>
+                            <p class="mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde voluptas,
+                                error aperiam
+                                voluptate non culpa magnam praesentium consectetur velit ipsa?</p>
+                            <a href="#" class="btn">Visit us today</a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            {{-- Our offer section --}}
+            <section id="our-offer">
+                <div class="container py-5">
+                    <div class="row">
+                        <div class="col">
+                            <div class="offer-box text-box text-center p-3">
+                                <h3 class="text-uppercase h6">lorem ipsum</h3>
+                                <h2 class="text-uppercase h4 mb-3">lorem ipsum</h2>
+                                <p class="mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta
+                                    ratione praesentium perferendis nihil nemo. Harum, ad eum! Possimus ipsa modi atque
+                                    et non maiores dolor soluta, eos harum, odit similique nam assumenda blanditiis
+                                    rerum debitis magnam? Ipsum dignissimos deleniti non tempora voluptates minima,
+                                    ducimus labore odio exercitationem fugit sunt unde dolorem perferendis omnis, vero
+                                    eum, vitae rem.</p>
+                                <a href="#" class="btn">Visit us today</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </main>
+
+        {{-- Footer --}}
+        <footer class="py-3">
+            <div class="container">
+                <div class="row">
+                    <div class="col text-center">
+                        <p class="mb-0">Copyright &copy; Your Website 2022</p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
